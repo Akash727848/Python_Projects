73f5d54e08c26130d3b1aafd5e69da064d582c94
Akash727848 <akashakshay75@gmail.com>
2024-06-02 08:01:22 +0530

    commit

--- Assignments/set_operations.py
+++ Assignments/set_operations.py
@@ -1,6 +1,11 @@
 s1={'a','k','a','s','h'}
 s2=set(s1)
-print(s2)
+#print(s2)
 s3={'m','o','u','n','i','c','a'}
-print(s3)
-print(s2.union)
+#print(s3)
+#print(s2.union(s3))
+s4=s2.intersection(s3)
+print(s4)
+s2.intersection_update(s3)
+print(s2)
+print(s5)
