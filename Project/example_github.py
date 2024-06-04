9eb919f13ec26d65d585910b8da108030ce75efa
Akash727848 <akashakshay75@gmail.com>
2024-06-04 10:36:37 +0530

    Update example_github.py

--- Project/example_github.py
+++ Project/example_github.py
@@ -1,3 +1,3 @@
 print("this is test Document 1")
-print("this doc was version 0.1 - 1.0")
-print("now this document is version 2.0")
+print("this doc was version 0.1 - 1.0 - 2.0")
+print("now this document is version 3.0")
